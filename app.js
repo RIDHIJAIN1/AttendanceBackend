b1026a0893c279c24e3acf46c33b1c5d3d6028bd
Ridhi Jain <110471382+RIDHIJAIN1@users.noreply.github.com>
2025-03-01 12:09:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, Vercel!");
-// });
+ app.get("/", (req, res) => {
+   res.send("Hello, Vercel!");
+ });
 
  module.exports = app;  
